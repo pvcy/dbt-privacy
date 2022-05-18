a0271d89ac05d6914f3eb245a7cf69fde48449a0
Ted Conbeer <tconbeer@users.noreply.github.com>
2022-05-18 15:07:35 -0600

    refactor: modularize test for safe_mask

--- integration_tests/models/dim_users.sql
+++ integration_tests/models/dim_users.sql
@@ -20,13 +20,7 @@ with
                 dbt_privacy.generate_pepper(
                     pepper_scope="project", pepper_persistence="per-run"
                 )
-            }} as ephemeral_project_pepper,
-            
-            {{ dbt_privacy.safe_mask("email") }} as email_safe_mask,
-            {{ dbt_privacy.safe_mask("email", mask_char="/") }} as email_safe_mask_slash,
-            {{ dbt_privacy.safe_mask("email", keep_n=4) }} as email_safe_mask_keep_4,
-            {{ dbt_privacy.safe_mask("email", n=8, keep_n=6, keep_dir="left") }} as email_safe_mask_keep_left,
-            {{ dbt_privacy.safe_mask("null") }} as null_safe_mask
+            }} as ephemeral_project_pepper
         from users
     )
 select *
